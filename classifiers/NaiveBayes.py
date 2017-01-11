9d12ad9b639b4ed7bf7c194d4ce0325affb34fa3
eytan <eytan.singher@gmail.com>
2017-01-11 20:10:04 +0200

    near the end
    Signed-off-by: eytan <eytan.singher@gmail.com>

--- classifiers/NaiveBayes.py
+++ classifiers/NaiveBayes.py
@@ -1,38 +1,40 @@
 import pandas
 from collections import defaultdict
 import argparse
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+import sklearn.base
 
 
-class NaiveBayes(object):
-    def __init__(self, priora=1.0/50000, data=None, classes=None):
-        self.data = data
-        self.classes = classes
+class NaiveBayes(sklearn.base.BaseEstimator):
+    def __init__(self, priora=1.0/50000):
+        self.data_ = None
+        self.classes_ = None
         # for key == y value is probability
-        self._init_inner()
         self.priora = priora
+        self.py_ = {}
+        self.pxiy_ = {}
+        self.regulisers_ = {}
 
+    def fit(self, X, y, copy=False):
+        self.classes_ = y
+        self.data_ = X
+        self.py_ = {}
+        self.pxiy_ = {}
+        self.regulisers_ = {}
 
-    def _init_inner(self):
-        self._py = {}
-        self._pxiy = {}
-        self._regulisers = {}
-
-    def train(self, copy=False):
-        if self.classes is None or self.data is None:
+        if self.classes_ is None or self.data_ is None:
             raise RuntimeError("Need to initialize date and classes before train")
         if copy:
-            res = NaiveBayes(self.priora, self.data, self.classes)
-            return res.train(copy=False)
+            res = NaiveBayes(self.priora)
+            return res.fit(X, y)
 
-        if not isinstance(self.data, pandas.DataFrame):
-            self.data = pandas.DataFrame(self.data)
-        if not isinstance(self.classes, pandas.Series):
-            self.classes = pandas.Series(self.classes)
-        self._init_inner()
+        if not isinstance(self.data_, pandas.DataFrame):
+            self.data_ = pandas.DataFrame(self.data_)
+        if not isinstance(self.classes_, pandas.Series):
+            self.classes_ = pandas.Series(self.classes_)
         # MLE calculation of py is number of seen divided by total number of samples
         # TODO: probably dont need to regularise value
-        self._py = self.classes.value_counts().map(lambda count: count/float(self.classes.count())).to_dict()
+        self.py_ = self.classes_.value_counts().map(lambda count: count / float(self.classes_.count())).to_dict()
 
         # MAP calculation of pxiy is a dictionary of y values then x values which is theta_j_i meaning in our case
         # P(X = x_i | Y=yj) where x_i is the i word from vocabulary and yj is the j classification
@@ -40,29 +42,28 @@ class NaiveBayes(object):
         # theta_j_i = count of xi with yj + prioria (alpha_i which is the same for all)
 
         # split data by classes and then just bag up the words. then we will have the wanted counts.
-        self.data['classification'] = self.classes
         y_data = {}
-        for yj in self._py.keys():
-            y_data[yj] = self.data.loc[self.data['classification'] == yj].drop('classification')
-            self._regulisers[yj] = sum(y_data[yj].count()) + self.priora*30
-            self._pxiy[yj] = defaultdict(lambda: self.priora)
+        for yj in self.py_.keys():
+            y_data[yj] = self.data_.loc[self.classes_ == yj]
+            self.regulisers_[yj] = sum(y_data[yj].count()) + self.priora * 30
+            self.pxiy_[yj] = defaultdict(lambda: self.priora)
             for c in y_data[yj]:
                 for i, count in y_data[yj][c].value_counts().iteritems():
-                    self._pxiy[yj][i] += count
+                    self.pxiy_[yj][i] += count
             # might end up huge when claculating probs so regulate the value a bit
-            for k in self._pxiy[yj]:
-                self._pxiy[yj][k] /= self._regulisers[yj]
+            for k in self.pxiy_[yj]:
+                self.pxiy_[yj][k] /= self.regulisers_[yj]
 
         return self
 
     def classify(self, data):
         max_prob = 0
         max_y = None
-        for y, prob in self._py.items():
+        for y, prob in self.py_.items():
             for w in data:
-                if w not in self._pxiy[y]:
-                    self._pxiy[y][w] /= self._regulisers[y]
-                prob *= self._pxiy[y][w]
+                if w not in self.pxiy_[y]:
+                    self.pxiy_[y][w] /= self.regulisers_[y]
+                prob *= self.pxiy_[y][w]
             if prob > max_prob:
                 max_prob = prob
                 max_y = y
@@ -77,11 +78,6 @@ class NaiveBayes(object):
             total += 1
             res += y == self.classify(data)
         return float(res)/total
-
-    def fit(self, x, y):
-        self.data = x
-        self.classes = y
-        self.train()
 
     def predict(self, x):
         return self.classify(x)
@@ -108,36 +104,9 @@ def __main__():
         priora += args.prior
         if priora > 1:
             continue
-        print("random set valiidation:")
-        for i in range(args.validations):
-            xtrain, xtest, ytrain, ytest = train_test_split(learning_data, classes, test_size=args.test/float(args.folds))
-            clf = NaiveBayes(priora=priora, data=xtrain, classes=ytrain)
-            clf.train()
-            print('\tprior: {}. validation: {}. score: {}'.format(priora, i+1, clf.score(xtest, ytest)))
-        print("kfold validations:")
-
-        for i in range(args.validations):
-            kf = KFold(args.folds, shuffle=True)
-            total = 0
-            length = 0
-            for train_indices, test_indices in kf.split(learning_data):
-                train_X = learning_data.loc[learning_data.index[train_indices]]
-                train_Y = classes.loc[learning_data.index[train_indices]]
-                test_X = learning_data.loc[learning_data.index[test_indices]]
-                test_Y = classes.loc[learning_data.index[test_indices]]
-
-                # Train the model, and evaluate it
-                clf = NaiveBayes(priora=priora, data=train_X, classes=train_Y)
-                clf.train()
-
-                predictions = [clf.classify(x[1]) for x in test_X.iterrows()]
-                testing_classes = [y for y in test_Y]
-                acc = sum(map(lambda x: x[0] == x[1], zip(predictions, testing_classes)))
-                total += float(acc)/len(test_Y)
-                length += 1
-            print("after {} runs got {} accuracy".format(length, total/length))
-
-
+        cv = 10
+        print("prior - {} . accuracy - {}".format(priora,
+                            sum(cross_val_score(NaiveBayes(priora=priora), learning_data, classes, cv=cv))/float(cv)))
 
 if __name__ == '__main__':
     __main__()
